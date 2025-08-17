823b13de40c91b8137d91af4cc4afc39d93cc871
JimsMowingNorthHobart <jimsmowingnorthhobart@gmail.com>
2025-08-17 22:46:55 +0545

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,3 @@
 self.addEventListener("install", e => self.skipWaiting());
 self.addEventListener("activate", e => self.clients.claim());
 self.addEventListener("fetch", e => {});
-<nav className="navbar">
-  <a href="#/">Dashboard</a>
-  <a href="#/clients">Clients</a>
-  <a href="#/calendar">Calendar</a>
-  <a href="#/map">Map</a>
-  <a href="#/settings">Settings</a>
-</nav>
