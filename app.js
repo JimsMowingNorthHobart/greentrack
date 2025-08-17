10f6552d77b64b396bca84f2d741598433d7c56a
JimsMowingNorthHobart <jimsmowingnorthhobart@gmail.com>
2025-08-17 22:42:50 +0545

    Create app.js

---
+++ app.js
@@ -0,0 +1,7 @@
+<nav className="navbar">
+  <a href="#/">Dashboard</a>
+  <a href="#/clients">Clients</a>
+  <a href="#/calendar">Calendar</a>
+  <a href="#/map">Map</a>
+  <a href="#/settings">Settings</a>
+</nav>
